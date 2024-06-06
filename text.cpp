3b26a6251e60beefe52ad4ebe99adc8f08df821e
Thurein42 <Thomasether142@gmail.com>
2024-06-06 11:49:15 +0630

    testing

---
+++ text.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a1,a2,ans;
+    cout<<"Enter first number: \n";
+    cin>>a1;
+    cout<<"Enter second number: \n";
+    cin>>a2;
+    ans = a1+a2;
+    cout<<"Result = "<<ans<<endl;
+}
